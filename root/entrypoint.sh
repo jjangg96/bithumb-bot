3dd3d461ac5d054c61fb792a8c0c5c9049706dfa
jjangg96 <jjangg96@gmail.com>
2024-01-06 19:01:53 +0900

    pm2 log 추가

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Update notes"
 git log --pretty=format:"%ch - %s"  | head --lines 4
 
-exec pm2-runtime /usr/local/bin/npm -- run start
+exec pm2-runtime /usr/local/bin/npm --output ./.pm2/logs/out.log --error ./.pm2/logs/error.log -- run start
