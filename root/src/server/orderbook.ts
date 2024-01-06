aa24dc078c296c4bba5a176308d2bc89b20c0192
jjangg96 <jjangg96@gmail.com>
2024-01-06 19:22:17 +0900

    서버 bid, ask 보낼 때 버그 수정

--- root/src/server/orderbook.ts
+++ root/src/server/orderbook.ts
@@ -51,7 +51,7 @@ export class Orderbook {
                 askTarget = this.targets[coin] && this.targets[coin].ask === ask ? ask : ask - tick;
             }
 
-            if(this.targets[coin] === undefined || this.targets[coin].bid != bidTarget || this.targets[coin].ask != askTarget) {
+            if (this.targets[coin] && (this.targets[coin].bid != bidTarget || this.targets[coin].ask != askTarget)) {
                 // console.log(coin, bidTarget, askTarget);
                 this.socket.emit(coin.toUpperCase(), this.targets[coin]);
             }
